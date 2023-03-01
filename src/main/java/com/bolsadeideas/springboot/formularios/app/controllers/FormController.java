f012576380071a964db5869eab954c992e44b719
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-28 23:19:11 -0600

    Clase Controlador "FormController"

--- src/main/java/com/bolsadeideas/springboot/formularios/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/formularios/app/controllers/FormController.java
@@ -1,8 +1,30 @@
 package com.bolsadeideas.springboot.formularios.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
+	
+	//	Metodos Handler
+	@GetMapping("/form")
+	public String form(Model model) {
+		
+		
+		return "form";
+	}
+	
+	
+	@PostMapping("/form")
+	public String procesar(Model model) {
+		
+		
+		return "form";
+	}
+	
 
+	
+	
 }
