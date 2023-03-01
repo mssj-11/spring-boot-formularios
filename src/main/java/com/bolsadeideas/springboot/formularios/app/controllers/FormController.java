2ae324608c231d1f3e9ce5aed1a2326cbb505c73
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-03-01 00:50:09 -0600

    Metodo handler para recibir los datos del Form

--- src/main/java/com/bolsadeideas/springboot/formularios/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/formularios/app/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -11,15 +12,24 @@ public class FormController {
 	//	Metodos Handler
 	@GetMapping("/form")
 	public String form(Model model) {
-		
+		model.addAttribute("titulo", "Formulario Usuarios");
 		
 		return "form";
 	}
 	
 	
 	@PostMapping("/form")
-	public String procesar(Model model) {
+	//	Capturando los parametros (username, password, email)
+	public String procesar(Model model, @RequestParam String username, 
+			@RequestParam String password, @RequestParam String email) {
 		
+		
+		model.addAttribute("titulo", "Resultado del Formulario");
+		
+		//	Mostrandolo en el HTML "resultado"
+		model.addAttribute("username", username);
+		model.addAttribute("password", password);
+		model.addAttribute("email", email);
 		
 		return "resultado";
 	}
