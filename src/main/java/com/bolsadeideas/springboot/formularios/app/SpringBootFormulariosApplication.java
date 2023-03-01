767ee9d5f4a7592f8b31a5c96a0809ea77ee93d0
Michael S.S <80673026+mssj-11@users.noreply.github.com>
2023-02-28 23:10:48 -0600

    Formularios en Spring Framework

---
+++ src/main/java/com/bolsadeideas/springboot/formularios/app/SpringBootFormulariosApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.formularios.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFormulariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFormulariosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bolsadeideas/springboot/formularios/app/controllers/FormController.java
@@ -0,0 +1,8 @@
+package com.bolsadeideas.springboot.formularios.app.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FormController {
+
+}
